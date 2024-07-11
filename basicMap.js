a52d6261d55e4ef4e3d6d93a78168ad7421a8411
samuelobele2007 <Sam12524939@gmail.com>
2024-07-11 16:38:40 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -1,1 +1,14 @@
 console.log("basicMap.js loaded.");
+
+// CPCC coordinates: 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
+
+let map = L.map("map" , {
+  center: {35.2176665, -80.831473},
+  zoom: 16
+});
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
