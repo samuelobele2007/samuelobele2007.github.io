6a037d0e4c8d5b695025278eb1b6a18ff191eb60
samuelobele2007 <Sam12524939@gmail.com>
2024-07-11 16:43:03 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -3,7 +3,7 @@ console.log("basicMap.js loaded.");
 // CPCC coordinates: 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
 
 let map = L.map("map" , {
-  center: {35.2176665, -80.831473},
+  center: [35.2176665, -80.831473],
   zoom: 16
 });
 
