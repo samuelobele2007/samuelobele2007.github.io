edc221e3df1a7e3b64502644f6553c068601b44a
samuelobele2007 <Sam12524939@gmail.com>
2024-07-16 15:51:09 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -12,3 +12,6 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+L.marker([ 35.2176665, -80.831473],{
+  title: "hello from CPCC!"}).addTo(map);
