c952e8d8178c8d5b4dc5f87127a799987098dd7e
samuelobele2007 <Sam12524939@gmail.com>
2024-07-18 15:37:22 -0400

    Update basicMap.js

--- basicMap.js
+++ basicMap.js
@@ -2,17 +2,11 @@ console.log("basicMap.js loaded.");
 
 // CPCC coordinates: 35.2176665 N, 80.831473 W -> 35.2176665, -80.831473
 
-let map = L.map("map" , {
-  center: [35.2176665, -80.831473],
-  zoom: 16
-});
 
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
 
+let markers = [];
+markers.push(
 L.marker([ 35.2176665, -80.831473],{
   title: "hello from CPCC!"})
   .bindPopup(`
@@ -25,4 +19,33 @@ L.marker([ 35.2176665, -80.831473],{
   <p>[URL: https://www.youtube.com/watch?v=9UWwLKjaPDQ]</p>
   </center>
   `)
-  .addTo(map);
+)
+
+let CPCC= L. layerGroup(markers);
+let street = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+})
+
+var topo = L.tileLayer('https://basemap.nationalmap.gov/arcgis/rest/services/USGSImageryTopo/MapServer/tile/{z}/{y}/{x}', {
+	maxZoom: 20,
+	attribution: 'Tiles courtesy of the <a href="https://usgs.gov/">U.S. Geological Survey</a>'
+});
+
+let baseMMaps = {
+  Street: street,
+  Topographic: topo
+}
+  let overlayMaps ={
+    CPCC: CPCC
+)
+
+  let map = L.map("map", {
+  center: [35.2176665, -80.831473],
+  zoom: 16,
+    layers: [street, CPCC]
+});
+
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+    ]).addTo(map) ;
